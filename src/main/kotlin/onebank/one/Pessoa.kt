d345a9066e024ee119cb982801ea412536a557fb
luiz-gabriel <luizgabriel515@gmail.com>
2021-07-27 11:26:06 -0300

    update to talk about class menbers

--- src/main/kotlin/onebank/one/Pessoa.kt
+++ src/main/kotlin/onebank/one/Pessoa.kt
@@ -6,6 +6,10 @@ class Pessoa {
     var cpf: String = "111.000.222-33"
 
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$name $cpf"
     /*
     inner class Endereco{
         var rua: String = "Rua teste"
@@ -17,7 +21,7 @@ fun main()
 {
     var bik = Pessoa()
 
-    println(bik.name)
-    println(bik.cpf)
+    println(bik.pessoaInfo())
+
     //println(bik.Endereco().rua)
 }
